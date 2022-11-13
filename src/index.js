978aa311648cb89f9e6acf7bf205566b3e5c816a
VitaliMay <mvt30@mail.ru>
2022-11-13 22:22:10 +0300

    feat: solution morse-decoder

--- src/index.js
+++ src/index.js
@@ -38,7 +38,19 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    
+    const ABC = {'**********': ' '}  // объявляем алфавит с цифровым ключём, учитывая что в Морзе нет пробела)
+    const digit = Object.keys(MORSE_TABLE) // достаём ключи из Морзе
+    for (let i = 0; i < digit.length; i++) {
+        // формируем цифровой алфавит из Морзе
+        ABC[('0000000000' + digit[i].replace(/\./g, '10').replace(/\-/g, '11')).slice(-10)] = MORSE_TABLE[digit[i]]
+    }
+    let text = '' // здесь будет формироваться результат декодирования
+    for (let i = 0; i < expr.length; i=i+10) {
+        text = text + ABC[expr.substring(i, i+10)] // декодируем входящую строку
+    }
+    return text
+
 }
 
 module.exports = {
